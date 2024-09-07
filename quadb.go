8bc2eb05002942c99574299d8bdcb08ecfa67330
Sos247 <stavvals@yahoo.com>
2024-09-07 14:10:03 +0300

    quadb ready, check it out

---
+++ quadb.go
@@ -0,0 +1,38 @@
+package piscine
+
+import "fmt"
+
+func QuadB(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+
+	for i := 0; i < y; i++ {
+		for j := 0; j < x; j++ {
+			if i == 0 && j == 0 {
+				// Upper left corner
+				fmt.Print("/")
+			} else if i == 0 && j == x-1 {
+				// Upper right corner
+				fmt.Print("\\")
+			} else if i == y-1 && j == 0 {
+				// Lower left corner
+				fmt.Print("\\")
+			} else if i == y-1 && j == x-1 {
+				// Lower right corner
+				fmt.Print("/")
+			} else if i == 0 || i == y-1 {
+				// Top or bottom border
+				fmt.Print("*")
+			} else if j == 0 || j == x-1 {
+				// Left or right border
+				fmt.Print("*")
+			} else {
+				// Inside of the rectangle, empty space
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+
+}
