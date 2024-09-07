c2462d66226d57de90f5d4b9fee10a16637b3e05
Sos247 <stavvals@yahoo.com>
2024-09-07 14:22:42 +0300

    quad d ready, chack it out

---
+++ quadd.go
@@ -0,0 +1,38 @@
+package piscine
+
+import "fmt"
+
+func QuadD(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+
+	for i := 0; i < y; i++ {
+		for j := 0; j < x; j++ {
+			if i == 0 && j == 0 {
+				// Upper left corner
+				fmt.Print("A")
+			} else if i == 0 && j == x-1 {
+				// Upper right corner
+				fmt.Print("C")
+			} else if i == y-1 && j == 0 {
+				// Lower left corner
+				fmt.Print("A")
+			} else if i == y-1 && j == x-1 {
+				// Lower right corner
+				fmt.Print("C")
+			} else if i == 0 || i == y-1 {
+				// Top or bottom border
+				fmt.Print("B")
+			} else if j == 0 || j == x-1 {
+				// Left or right border
+				fmt.Print("B")
+			} else {
+				// Inside of the rectangle, empty space
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+
+}
