dd1fd0ba674dd1f77798ffae4fa69e5a045b3cdb
Sos247 <stavvals@yahoo.com>
2024-09-08 13:28:40 +0300

    Renamed files

--- quad/main.go
+++ quad/main.go
@@ -5,9 +5,25 @@ import (
 )
 
 func main() {
-	piscine.QuadA(5,3)
-	piscine.QuadA(5,1)
-	piscine.QuadA(1,1)
-	piscine.QuadA(1,5)
+	piscine.QuadA(5, 3)
+	piscine.QuadA(5, 1)
+	piscine.QuadA(1, 1)
+	piscine.QuadA(1, 5)
+
+	piscine.QuadE(5, 3)
+	piscine.QuadE(5, 1)
+	piscine.QuadE(1, 1)
+	piscine.QuadE(1, 5)
+
+	piscine.QuadB(5, 3)
+	piscine.QuadB(5, 1)
+	piscine.QuadB(1, 1)
+	piscine.QuadB(1, 5)
+
+	piscine.QuadD(5, 3)
+	piscine.QuadD(1, 5)
+	piscine.QuadD(1, 1)
+	piscine.QuadD(1, 1)
+	piscine.QuadD(1, 5)
 
 }
--- quadC.go
+++ quadC.go
@@ -9,25 +9,25 @@ func QuadC(x, y int) {
 	for i := 0; i < y; i++ {
 		for j := 0; j < x; j++ {
 			if i == 0 && j == 0 {
-				
+
 				fmt.Print("A")
 			} else if i == 0 && j == x-1 {
-				
+
 				fmt.Print("A")
 			} else if i == y-1 && j == 0 {
-				
+
 				fmt.Print("C")
 			} else if i == y-1 && j == x-1 {
-				
+
 				fmt.Print("C")
 			} else if i == 0 || i == y-1 {
-				
+
 				fmt.Print("B")
 			} else if j == 0 || j == x-1 {
-				
+
 				fmt.Print("B")
 			} else {
-				
+
 				fmt.Print(" ")
 			}
 		}
